dbe1abba434f3f7419e151ad2b405b7b427bb4c5
AshishBiswas-2027 <ab23ceb0b07@student.nitw.ac.in>
2025-10-22 10:50:09 +0530

    created a database amd defined the schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { middleware } from "./middleware";
-import { CreateUserSchema } from "@repo/common/types"
+import { CreateUserSchema , SigninSchema , CreateRoomSchema } from "@repo/common/types"
 
 const app = express();
 
@@ -24,6 +24,14 @@ app.post('/signup', (req, res) => {
 
 app.post('/signin', (req, res) => {
 
+    const data = SigninSchema.safeParse(req.body)
+    if (!data.success) {
+        res.json({
+            message :"Incorrect inputs"
+        })
+        return
+    }
+
 
     const userId = 1
     const token = jwt.sign({
@@ -37,7 +45,16 @@ app.post('/signin', (req, res) => {
 
     
     
- app.post('/room', middleware, (req, res) => {
+app.post('/room', middleware, (req, res) => {
+     
+     const data = CreateRoomSchema.safeParse(req.body)
+    if (!data.success) {
+        res.json({
+            message :"Incorrect inputs"
+        })
+        return
+    }
+
     res.json({ roomId: 123 })
 })
     
